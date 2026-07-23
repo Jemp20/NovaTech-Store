684c6833b2aa1ea1bc4e62f51dfe029ca81e484a
Joel <joelby1234@gmail.com>
2026-07-22 21:50:29 -0500

    Agregar app.listen para que el servidor arranque

--- server/server.js
+++ server/server.js
@@ -49,4 +49,10 @@ app.get('/', (req, res) => {
     });
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
 module.exports = app;
